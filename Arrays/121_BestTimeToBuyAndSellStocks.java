290aacf4fe95f8bc91dc280e3e18641d97df5a8e
Kunal Vitthal Barakade <161806704+Kunal-Barakade@users.noreply.github.com>
2026-07-17 00:43:42 +0530

    Create 121_BestTimeToBuyAndSellStocks.java

---
+++ Arrays/121_BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0] ;
+        int max = 0 ;
+
+        for(int i = 1 ; i < prices.length ; i++)
+        {
+            int p = prices[i]-min ;
+            if(p > max) max = p ;
+            if(prices[i] < min) min = prices[i] ;
+        }
+        return max ;
+    }
+}
